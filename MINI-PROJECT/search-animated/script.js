8c73ccd9be6b651071d04a885432dbc653c09d46
bennydrspc <bennydrm16@gmail.com>
2020-10-16 10:07:43 +0700

    fix search data in search animated

--- MINI-PROJECT/search-animated/script.js
+++ MINI-PROJECT/search-animated/script.js
@@ -1,29 +1,29 @@
-const searchBox = document.querySelector(".search-box");
-const searchBtn = document.querySelector(".search-icon");
-const cancelBtn = document.querySelector(".cancel-icon");
-const searchInput = document.querySelector("input");
-const searchData = document.querySelector(".search-data");
+const searchBox = document.querySelector(".search-box")
+const searchBtn = document.querySelector(".search-icon")
+const cancelBtn = document.querySelector(".cancel-icon")
+const searchInput = document.querySelector("input")
+const searchData = document.querySelector(".search-data")
 
 searchBtn.addEventListener('click', () => {
-  searchBox.classList.add("active");
-  searchBtn.classList.add("active");
-  searchInput.classList.add("active");
-  cancelBtn.classList.add("active");
-  searchInput.focus();
+  searchBox.classList.add("active")
+  searchBtn.classList.add("active")
+  searchInput.classList.add("active")
+  cancelBtn.classList.add("active")
+  searchInput.focus()
   if (searchInput.value != "") {
-    var values = searchInput.value;
-    searchData.classList.remove("active");
-    searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
+    var values = searchInput.value
+    searchData.classList.remove("active")
+    searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>"
   } else {
-    searchData.textContent = "";
+    searchData.textContent = ""
   }
 })
 
 cancelBtn.addEventListener('click', () => {
-  searchBox.classList.remove("active");
-  searchBtn.classList.remove("active");
-  searchInput.classList.remove("active");
-  cancelBtn.classList.remove("active");
-  searchData.classList.toggle("active");
-  searchInput.value = "";
+  searchBox.classList.remove("active")
+  searchBtn.classList.remove("active")
+  searchInput.classList.remove("active")
+  cancelBtn.classList.remove("active")
+  searchData.classList.add("active")
+  searchInput.value = ""
 })
